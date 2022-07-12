6dbf31acee113ad99d968a62c7b9d6a09332e8e6
Aditi Sudhir <aditisudhir@Aditis-MacBook-Pro.local>
2022-07-12 18:03:11 +0530

    inital commit

---
+++ app/src/main/java/com/example/gfgretrofit/RetrofitAPI.java
@@ -0,0 +1,18 @@
+package com.example.gfgretrofit;
+
+import retrofit2.Call;
+import retrofit2.http.Body;
+import retrofit2.http.POST;
+
+public interface RetrofitAPI {
+
+        // as we are making a post request to post a data
+        // so we are annotating it with post
+        // and along with that we are passing a parameter as users
+        @POST("users")
+
+        //on below line we are creating a method to post our data.
+        Call<DataModal> createPost(@Body DataModal dataModal);
+    }
+
+
